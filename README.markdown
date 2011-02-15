7fad94704da304b3b221aaec5ebb06e3b0ab5529
Mark Scholtz <mark.scholtz@unboxedconsulting.com>
2011-02-15 08:03:07 +0200

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,9 @@
+The app was created with Rails 3.0.4 with the following command:
+================================================================
+* rails new shopmunki_web_v0.1 -J -T -d mysql
+
+Do the following to get the app up and running on a new machine:
+================================================================
+* bundle install
+
+TODO: document how the setup process would work on a new machine
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopmunkiWebV01::Application.config.secret_token = '67e3c55165daf278184defd569c4394fd3219872bfdf878f70f756a7032fc6baf6c41e57c319b9b90f7b929c7e8ed23b412f673304e5bd83a1794a89227fae34'
